0470bb4aa87ec499c45a87c7b16f989f845ce228
Nakul Upadhyay <upadhyaynakul559@gmail.com>
2024-01-15 20:12:04 +0530

    Final commit

--- src/textSummarizer/logging/__init__.py
+++ src/textSummarizer/logging/__init__.py
@@ -2,25 +2,21 @@ import os
 import sys
 import logging
 
-# Logging configuration
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 log_dir = "logs"
-log_filepath = os.path.join(log_dir, "running_logs.log")
+log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
-# Create a custom logger
+
+
+logging.basicConfig(
+    level= logging.INFO,
+    format= logging_str,
+
+    handlers=[
+        logging.FileHandler(log_filepath),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
 logger = logging.getLogger("textSummarizerLogger")
-logger.setLevel(logging.INFO)
-
-# Create handlers
-file_handler = logging.FileHandler(log_filepath)
-console_handler = logging.StreamHandler(sys.stdout)
-
-# Set format for handlers
-formatter = logging.Formatter(logging_str)
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
-
-# Add handlers to the logger
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
--- template.py
+++ template.py
@@ -2,14 +2,15 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO,format='[%(asctime)s]:%(message)s:')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
 
 project_name = "textSummarizer"
 
-list_of_file = [
+list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
@@ -25,20 +26,28 @@ list_of_file = [
     "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "test.py"
+
 ]
 
-for filepath in list_of_file:
+
+for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-    if filedir !="":
-        os.makedirs(filedir,exist_ok=True)
-        logging.info(f"creating directory:{filedir} for the file {filename}")
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
     
-    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
-            logging.info(f'creating empty file: {filepath}')
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    
     else:
-        logging.info(f'{filepath} is already exist')
+        logging.info(f"{filename} is already exists")
+
